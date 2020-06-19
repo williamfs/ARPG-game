32362ff1cf06f6e02cb896621f23c0a593f35028
victor0710-hub <64790857+victor0710-hub@users.noreply.github.com>
2020-06-18 22:19:43 -0500

    Dialogue System
    Done with dialogue system.

--- ARPG/Assets/Dialogue_Scripts/General_Controller.cs
+++ ARPG/Assets/Dialogue_Scripts/General_Controller.cs
@@ -9,9 +9,13 @@ public class General_Controller : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] TextMeshProUGUI Something;
     [SerializeField] GameObject Text_As_GO;
-    
+
+    private Collision2D person;
+
     int Index = 0;
     int total_count;
+    string Dialoge;
+    bool start = false;
     void Start()
     {
 
@@ -22,18 +26,39 @@ public class General_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Something.text = Dialoge;
+        if (start == true)
+        {
+            Dialoge = person.gameObject.GetComponent<Dialogues>().Return_Dialogues(Index);
+            Debug.Log(Index);
+        }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        string Dialoge = collision.gameObject.GetComponent<Dialogues>().Return_Dialogues(Index);
-        total_count = collision.gameObject.GetComponent<Dialogues>().TotalString();
+        // Dialoge = collision.gameObject.GetComponent<Dialogues>().Return_Dialogues(Index);
+        Text_As_GO.SetActive(true);
+        person = collision;
+        total_count = collision.gameObject.GetComponent<Dialogues>().TotalString()-1;
+        start = true;
         
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        start = false;
+        Index = 0;
+        Text_As_GO.SetActive(false);
+    }
+
     public void  NextNumber()
     {
         if (Index<total_count) {
             Index++;
+          
+        }
+        else
+        {
+            Text_As_GO.SetActive(false);
         }
     }
 }
