a9e9c31930c2378e0d9fd509f187d0371e2dee02
williamfs <CSheu@student.fullsail.edu>
2020-06-24 17:43:08 -0400

    Simple day night cycle version 1

---
+++ ARPG/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] float cycleSpeed = 2;
+    public Light2D light2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light2D = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light2D.intensity -= Time.deltaTime * cycleSpeed;
+        if (light2D.intensity <= 0 || light2D.intensity >= 1)
+        {
+            cycleSpeed *= -1;
+        }
+    }
+}
