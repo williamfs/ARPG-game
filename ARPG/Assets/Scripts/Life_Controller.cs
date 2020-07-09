4496fd04fed441a5f2c8f99fd5bfb259c35a0129
victor0710-hub <64790857+victor0710-hub@users.noreply.github.com>
2020-07-08 19:31:45 -0500

    Light system
    Light system and life system added.

---
+++ ARPG/Assets/Scripts/Life_Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Life_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Sprite[] heart_Sprites;
+
+
+    SpriteRenderer HeartRender;
+
+    int counter = 0;
+    int totalSprites;
+    bool Corutine_lock = false;
+    void Start()
+    {
+        totalSprites = heart_Sprites.Length;
+        HeartRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q) && Corutine_lock ==false)
+        {
+            StartCoroutine(HeartCicle());
+        }
+    }
+    IEnumerator HeartCicle()
+    {
+        yield return new WaitForSeconds(2f);
+
+        if (counter < totalSprites)
+
+        {
+            HeartRender.sprite = heart_Sprites[counter];
+            counter++;
+        }
+
+
+    }
+}
---
+++ ARPG/Assets/Scripts/Light_Controller.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UIElements;
+
+public class Light_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] double Timer = 0.1;
+    [SerializeField] Light2D PlayerLight;
+    [SerializeField] Transform LightBar;
+    [SerializeField] float total_Bateries = 2f;
+    [SerializeField] TextMeshProUGUI Batery_text;
+    
+    double counter = 1f;
+    bool lightOn = false;
+    bool lock_corutine = false;
+    void Start()
+    {
+        PlayerLight = GetComponent<Light2D>();
+        Batery_text.text = total_Bateries.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        keyPressed();
+        StartLight();
+        CheckBateries();
+ 
+    }
+    private void keyPressed()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            lightOn = !lightOn;
+        }
+    }
+    private void StartLight()
+    {
+        if(lightOn&&counter>0)
+        {
+            PlayerLight.pointLightOuterRadius = 10;
+            counter -= Time.deltaTime * Timer;
+            LightBar.transform.localScale = new Vector2((float)counter, 1f);
+        }
+        else if(counter<0)
+        {
+            PlayerLight.pointLightOuterRadius = 0;
+        }
+    
+    }
+    private void CheckBateries()
+    {
+      
+        if(LightBar.transform.localScale.x <=0 && lock_corutine ==false&&total_Bateries>0)
+        {
+            lock_corutine = true;
+            StartCoroutine(RechargeBatery());
+        }
+    }
+    IEnumerator RechargeBatery()
+    {
+        yield return new WaitForSeconds(3f);
+        LightBar.transform.localScale = new Vector2(1f,1f);
+        total_Bateries--;
+        lock_corutine = false;
+        counter = 1f;
+        Batery_text.text = total_Bateries.ToString();
+
+    }
+   
+}
